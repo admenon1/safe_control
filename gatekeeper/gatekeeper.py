e0044d05d9fb5bc301ad5a2a21ac41910e9a2c49
triakshunn <trivediakshunn@gmail.com>
2025-12-01 20:17:18 -0500

    checkout commit

--- gatekeeper/gatekeeper.py
+++ gatekeeper/gatekeeper.py
@@ -151,7 +151,7 @@ class Gatekeeper:
             # Generate the candidate trajectory using the nominal and backup controllers
             current_state = self.robot.X
             nominal_x_traj, nominal_u_traj = self._generate_nominal_trajectory(current_state, goal, discounted_nominal_horizon)
-        print(f"Generated nominal traj inside func: {nominal_x_traj.shape}") # Shape : (121,4) # assume 121 are the future time steps
+        #print(f"Generated nominal traj inside func: {nominal_x_traj.shape}") # Shape : (121,4) # assume 121 are the future time steps
         state_at_backup = nominal_x_traj[-1]  # last state of the nominal trajectory
         backup_x_traj, backup_u_traj = self._generate_backup_trajectory(state_at_backup, goal, self.backup_horizon)
 
@@ -176,21 +176,15 @@ class Gatekeeper:
         
         if unsafe_region is None or len(unsafe_region) == 0:
             return True
-        print(f"Unsafe region shape: {unsafe_region.shape if unsafe_region is not None else 'None'}")
-        print(f"unsafe_region: {unsafe_region}") ## ball_params['x0'], ball_params['y0'], ball_params['radius'],ball_params['vx'], 0.0, 0.0, 0.0
-        print(f"candidate_x_traj shape: {candidate_x_traj.shape}")
-        print(f"candidate_x_traj: {candidate_x_traj}")  ### n_steps,4 (x,y,theta,v) (maybe)
+        # print(f"Unsafe region shape: {unsafe_region.shape if unsafe_region is not None else 'None'}")
+        # print(f"unsafe_region: {unsafe_region}") ## ball_params['x0'], ball_params['y0'], ball_params['radius'],ball_params['vx'], 0.0, 0.0, 0.0
+        # print(f"candidate_x_traj shape: {candidate_x_traj.shape}")
+        # print(f"candidate_x_traj: {candidate_x_traj}")  ### n_steps,4 (x,y,theta,v) (maybe)
 
         for t,state in enumerate(candidate_x_traj):
 
             t=t*self.dt  ### since t is t/self.dt, need to convert back to time in seconds
-
-        
-        # if unsafe region is None or empty, return True
-           
-            # todo: roll out the obstacle for time t in future and check collision for those states. 
-            # Check if the candidate trajectory is within the safe region
-            #for state in candidate_x_traj:   ### this should remain unchanged. 
+            #### note: below code assumes the obstacle is moving with constant velocity in x direction only
             unsafe_region_t=unsafe_region.copy()
             unsafe_region_t[:,0]=unsafe_region[:,0]+t*unsafe_region[:,3] # x0 + vx * t
             for obs in unsafe_region_t: ### what is unsafe region? 
@@ -200,7 +194,7 @@ class Gatekeeper:
 
     def _update_committed_trajectory(self, discounted_nominal_horizon):
         """
-        Update the committed trajectory with the candidate trajectory.
+        Update the committed trajectory with the
         """
         self.committed_x_traj = self.candidate_x_traj
         self.committed_u_traj = self.candidate_u_traj
